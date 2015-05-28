198831ba72b49b4ede893549e214dbd61b886887
aramis <aramis.bsi@gmail.com>
2015-05-28 13:40:55 -0300

    Criação HTML
    Criação da base do HTML, inserido os estilos a serem usados, e as
    bibliotecas js

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-
-	header('Content-tyoe: text/html; charset=utf-8');
-
-
 	/*
 	*	Criação da constante mágica do php para os diretorios assim não sendo 
 	* necessário a criação de require_once desnecessário, e facilitando
@@ -25,12 +21,91 @@
 		}
 	}
 
-	
-
-	$t = new classTeste();
-	$t->nome();
-	
-	
-
 
 ?>
+<!DOCTYPE html>
+
+<html>
+	<head lang="pt-BR">
+		<title>Agenda PHP OO + AngularJS</title>
+		<meta charset="utf-8">
+
+		<!-- Arquivos CSS -->
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+		
+		
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> 
+					 	<span class="sr-only">Toggle navigation</span>
+					 	<span class="icon-bar"></span>
+					 	<span class="icon-bar"></span>
+					 	<span class="icon-bar"></span>
+					 </button> 
+					 <a class="navbar-brand" href="#">Agenda KA</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="#">Dashboard</a>
+						</li>
+						<li>
+							<a href="#">Clientes</a>
+						</li>						
+						<li>
+							<a href="#">Agenda</a>
+						</li>
+						<li>
+							<a href="#">Sair</a>
+						</li>
+					</ul>
+					<form class="navbar-form navbar-right" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" />
+						</div> <button type="submit" class="btn btn-default">Pesquisar</button>
+					</form>
+
+				</div>
+				</div>
+			</nav>
+
+
+			<div class="container">
+				<div class="row clearfix">
+					<div class="col-md-12 column">
+						<p>Conteudo</p>
+					</div>
+				</div>
+			</div>
+			
+			<footer class="footer">
+				<div class="container	">
+
+					<div class="col-md-12 column">
+						<p>Desenvolvido por Agência KA</p>
+					</div>
+				</div>
+			</footer>
+
+
+
+		<!-- Arquivos JS -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+
+	</body>
+
+</html>
+
+
+
+
+
